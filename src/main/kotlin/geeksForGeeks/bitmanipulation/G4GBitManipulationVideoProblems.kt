bdd9c2c4cd3d68f1ecd12f75df12be4cc95fa1bb
Akashkroy69 <Akashkroy@hotmail.com>
2021-06-01 20:48:31 +0530

    Added solution to find number of set bits with BRIAN KARNIGHAN ALGORITHM

--- src/main/kotlin/geeksForGeeks/bitmanipulation/G4GBitManipulationVideoProblems.kt
+++ src/main/kotlin/geeksForGeeks/bitmanipulation/G4GBitManipulationVideoProblems.kt
@@ -24,4 +24,32 @@ class G4GBitManipulationVideoProblems {
         }
         return setBitCount
     }
+    /* ^ the solution takes O(number-of-nits-in-the-given-number) to find set bits
+    * We can reduce it to O(number-of-set-bits) by using BRIAN KARNIGHAN ALGORITHM
+    * Brian Karnighan algorithm says: n & n-1 will make exactly one set bit, the first set bit found from right, as 0
+    * example: n= 110100
+    *            -     1
+    *           ----------
+    * then n-1 =  110011
+    * n & n-1 =   110000
+    *
+    * so in one iteration the algorithm finds and makes exactly one set bit as zero
+    * so by repeating the process until n becomes zero will lead us to the number of set bits
+    *
+    * */
+
+    fun numberofSetBitsBrianKarnighanAlgorthm(number: Int): Int {
+        var tempNumber = number
+        var setBitCount = 0
+        while (number > 0) {
+            number = number and number - 1
+            setBitCount++
+        }
+        return setBitCount
+    }
+}
+
+fun main() {
+    val setBCount = G4GBitManipulationVideoProblems().numberofSetBitsBrianKarnighanAlgorthm(3)
+    printl(setBCount)
 }
