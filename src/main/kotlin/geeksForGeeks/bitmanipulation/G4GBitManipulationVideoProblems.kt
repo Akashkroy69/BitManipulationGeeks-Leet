3560cb7d4587faf1df75a1474ce231a7c7eb538e
Akashkroy69 <Akashkroy@hotmail.com>
2021-06-01 20:05:01 +0530

    wrote for://problem 3: Given a number find how many set bits are there in the binary representation of the number.

--- src/main/kotlin/geeksForGeeks/bitmanipulation/G4GBitManipulationVideoProblems.kt
+++ src/main/kotlin/geeksForGeeks/bitmanipulation/G4GBitManipulationVideoProblems.kt
@@ -1,6 +1,7 @@
 package geeksForGeeks.bitmanipulation
 
 class G4GBitManipulationVideoProblems {
+    //Problem 1 : find whether the kth bit is set or not
     fun kThBitSetOrNot(number: Int, k: Int) {
         if (number and (1 shl k - 1) != 0) {
             println("The Kth bit is set")
@@ -8,5 +9,19 @@ class G4GBitManipulationVideoProblems {
             println("The Kth bit is not set")
 
         }
+    }
+
+    //problem 2 : Find whether the number is power of two or not?
+    fun powerOf2(number: Int) = number > 0 && (number and number - 1) == 0
+
+    //problem 3: Given a number find how many set bits are there in the binary representation of the number.
+    fun numberOfSetBits(number: Int): Int {
+        var locNum = number
+        var setBitCount = 0
+        while (locNum > 0) {
+            locNum = locNum shr 1
+            setBitCount++
+        }
+        return setBitCount
     }
 }
